bfae61fb009e883ad7da3585e92954c7a571f2ac
rakib <rakib.hasan814563@gmail.com>
2022-06-20 16:27:24 +0600

    con

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -13,4 +13,40 @@ exports.createContact = asyncHandler(async(req, res, next)=>{
         success: true,
         data: contact
     })
+});
+
+//@desc      Get all users contacts
+//@route     api/v1/contacts
+//@access    Private
+exports.getAllContacts = asyncHandler(async(req, res) =>{
+    const contacts = await Contact.find({user: req.user.id}).sort({name: 1});
+    res.status(200).json({
+        contacts
+    });
+});
+
+//@desc      Delete contact
+//@route     DELETE api/contacts/:id
+//@access    Private
+exports.deteleContact = asyncHandler(async(req, res) =>{
+    const contact = await Contact.findById(req.params.id);
+
+    if(!contact){
+        return res.status(404).json({
+            success: false,
+            msg: 'Contact not found'
+        })
+    }
+
+    if(contact.user.toString()!== req.user.id){
+        return res.status(404).json({
+            success: false,
+            msg: 'Not authorized'
+        })
+    }
+
+    await Contact.findByIdAndDelete(req.params.id);
+    res.json({
+        msg: 'contact deleted'
+    })
 })
--- routes/contacts.js
+++ routes/contacts.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const User = require("../models/Contact");
 const {protect} = require("../middleware/auth");
-const { createContact } = require("../controllers/contacts");
+const { createContact, getAllContacts, deteleContact } = require("../controllers/contacts");
 
 const router = express.Router();
 
 router.post('/createcontact', protect, createContact);
+router.get('/contacts', protect, getAllContacts);
+router.delete('/contact/:id', protect, deteleContact);
 
 module.exports = router;
