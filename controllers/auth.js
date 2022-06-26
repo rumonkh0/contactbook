1cbfc117fd6a65a95da63d0090665e884f29c1d4
rakib <rakib.hasan814563@gmail.com>
2022-06-27 00:22:43 +0600

    env error fix

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,130 +1,123 @@
-const asyncHandler = require('../middleware/async');
-const User = require('../models/User');
+const asyncHandler = require("../middleware/async");
+const User = require("../models/User");
 
 //@desc     Register user
 //@route    POST api/v1/auth/register
 //@access   public
-exports.register = asyncHandler(async(req, res, next) => {
-    const user = await User.create(req.body);
+exports.register = asyncHandler(async (req, res, next) => {
+  const user = await User.create(req.body);
 
-    res.status(201).json({
-        success: true,
-        data: user
-    });
+  res.status(201).json({
+    success: true,
+    data: user,
+  });
 
-    sendTokenResponse(user, res);
+  sendTokenResponse(user, res);
 });
 
 //@desc   Login user
 //@Route  POST api/v1/auth/login
 //@acess  public
-exports.login = asyncHandler(async (req, res, next) =>{
-    const {email, password} = req.body;
+exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
 
-    if(!email || !password){
-        return res.status(400).json({
-            error: 'Please enter email and password' 
-        });
-    }
+  if (!email || !password) {
+    return res.status(400).json({
+      error: "Please enter email and password",
+    });
+  }
 
-    //check for user
-    const user = await User.findOne({email}).select('+password');
+  //check for user
+  const user = await User.findOne({ email }).select("+password");
 
-    if(!user){
-        return res.status(400).json({
-            error: 'User not found' 
-        });
-    }
-   
+  if (!user) {
+    return res.status(400).json({
+      error: "User not found",
+    });
+  }
 
-    const isMatch = await user.matchPassword(password);
+  const isMatch = await user.matchPassword(password);
 
-    if(!isMatch){
-        return res.status(400).json({
-            error: 'Password does not match' 
-        })
-    }
+  if (!isMatch) {
+    return res.status(400).json({
+      error: "Password does not match",
+    });
+  }
 
-    sendTokenResponse(user, res);
-
-})
-
+  sendTokenResponse(user, res);
+});
 
 //@desc   get me
 //@route  GET api/v1/auth/me
 //@access privet
-exports.getMe = asyncHandler(async(req, res) => {
-    const user = await User.findById(req.user.id);
+exports.getMe = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user.id);
 
-    res.status(200).json({
-        success: true,
-        data: user
-    });
+  res.status(200).json({
+    success: true,
+    data: user,
+  });
 });
 
 //@desc   Logout user
 //@Route  GET api/v1/auth/logout
 //@acess  public
-exports.logout = asyncHandler(async(req, res, next) =>{
-    res.cookie('token', 'none', {
-        expires: new Date(Date.now() + 2*1000),
-        httponly: true
-    });
+exports.logout = asyncHandler(async (req, res, next) => {
+  res.cookie("token", "none", {
+    expires: new Date(Date.now() + 2 * 1000),
+    httponly: true,
+  });
 
-    res.status(200).json({
-        success: true,
-        data: {}
-    });
+  res.status(200).json({
+    success: true,
+    data: {},
+  });
 });
-
-
-
-
 
 //@desc   update user details
 //@route  PUT api/v1/auth/updatedetails
 //@access private
-exports.updateDetails = asyncHandler(async(req, res)=>{
-    const user = await User.findByIdAndUpdate(req.user.id, req.body);
-    res.status(200).json({
-        success: true,
-        data: user,
-      });
-})
+exports.updateDetails = asyncHandler(async (req, res) => {
+  const user = await User.findByIdAndUpdate(req.user.id, req.body);
+  res.status(200).json({
+    success: true,
+    data: user,
+  });
+});
 
 //@desc    update password
 //@route   api/v1/auth/updatepassword
 //@access  private
-exports.updatePassword = asyncHandler(async(req, res) =>{
-    const user = await User.findById(req.user.id).isSelected('+password');
+exports.updatePassword = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user.id).isSelected("+password");
 
-    if(!await User.matchPassword(req.password)){
-        return res.status(400).json({
-            success: false,
-            msg: 'invalid credential'
-        })
-    }
+  if (!(await User.matchPassword(req.password))) {
+    return res.status(400).json({
+      success: false,
+      msg: "invalid credential",
+    });
+  }
 
-user.password = req.body.newpassword;
+  user.password = req.body.newpassword;
 
-await user.save();
+  await user.save();
 
-sendTokenResponse(user, res);
-
+  sendTokenResponse(user, res);
 });
 
-
 const sendTokenResponse = (user, res) => {
-    //create token
-    const token = user.getSignedJwtToken();
+  //create token
+  const token = user.getSignedJwtToken();
 
-    const options = {
-        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRE*24*60*60*1000),
-        httponly: true
-    }
+  const options = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+    ),
+    httponly: true,
+  };
 
-    res.status(200).cookie('token', token, options).json({
-        success: true,
-        token
-    })
-}
+  res.status(200).cookie("token", token, options).json({
+    success: true,
+    token,
+  });
+};
